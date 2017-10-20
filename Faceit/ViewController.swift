c29d6296182b877179fc245de4a528fc5048b92f
latne1ec <latne1ec@cmich.edu>
2017-10-20 16:09:40 -0400

    First commit

---
+++ Faceit/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by Evan Latner on 10/20/17.
+//  Copyright © 2017 levellabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+}
+
